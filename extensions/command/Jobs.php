e55da0cb082ed9003ca1b640d5305d8c447a9d23
Christopher Garvis <cgarvis@gmail.com>
2011-06-14 18:34:37 -0400

    implemented console command

---
+++ extensions/command/Jobs.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+ 
+namespace li3_delayed_job\extensions\command;
+
+use li3_delayed_job\models\Jobs as DelayedJobs;
+use li3_delayed_job\models\Workers;
+
+/**
+ * Delayed_job (or DJ) encapsulates the common pattern of asynchronously
+ * executing longer tasks in the background.
+ */
+class Jobs extends \lithium\console\Command {
+  /**
+   * Clear the delayed_job queue.
+   */
+  public function clear() {
+    DelayedJobs::deleteAll();
+  }
+  
+  /**
+   * Start a delayed_job worker.
+   *
+   * @param $quiet          bool
+   * @param $min_priority   int
+   * @param $max_priority   int
+   */
+  public function work($quiet = false, $min_priority = 0, $max_priority = 0) {
+    $worker = new Workers(compact('quiet', 'min_priority', 'max_priority'));
+    $worker->start();
+  }
+}
